fd7a85b61ec6b7f53fb14a6c26ce463b9024558d
yearn <yang_xuanyi@163.com>
2017-07-22 23:05:25 +0800

    更改包名

---
+++ src/main/java/org.quick4.framework/annotation/Aspect.java
@@ -0,0 +1,17 @@
+package org.quick4.framework.annotation;
+
+import java.lang.annotation.*;
+
+/**
+ * 切面注解
+ */
+@Target(ElementType.TYPE)
+@Retention(RetentionPolicy.RUNTIME)
+public @interface Aspect {
+
+    /**
+     * 注解
+     */
+    Class<? extends Annotation> value();
+
+}
